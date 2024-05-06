5f17691a090ac7a95e4ae6027420195b9abd0ccd
Hakimuddin Kika <hakimuddin.kika92@gmail.com>
2024-05-06 18:48:12 +0300

    Setting up Workers for Redis to Pop the Queues requests

---
+++ worker/src/index.ts
@@ -0,0 +1,19 @@
+import { createClient } from "redis";
+
+const client = createClient();
+
+async function main(){
+    await client.connect()
+
+    while(1){
+        const response = await client.brPop("submissions",0);
+        console.log(response);
+        //actually run the users code docker exec
+        await new Promise((resolve) => setTimeout(resolve,1000))
+
+        //Send it to the PubSub
+        console.log("Processed users submission");
+    }
+}
+
+main()
